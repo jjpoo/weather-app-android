0f14c74346b1780f46c5ae3000b0ee9c6e795e9b
polina <ff@gmail.com>
2025-04-08 22:12:03 +0200

    fixes

--- app/src/main/java/com/polina/android/weather/app/data/models/WeatherForecastResponse.kt
+++ app/src/main/java/com/polina/android/weather/app/data/models/WeatherForecastResponse.kt
@@ -1,11 +1,14 @@
 package com.polina.android.weather.app.data.models
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class WeatherForecastResponse(
     val list: List<ForecastItem>
 )
 
+@JsonClass(generateAdapter = true)
 data class ForecastItem(
     val dt: Long,
     val main: Main,
--- app/src/main/java/com/polina/android/weather/app/data/models/Wind.kt
+++ app/src/main/java/com/polina/android/weather/app/data/models/Wind.kt
@@ -1,5 +1,8 @@
 package com.polina.android.weather.app.data.models
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class Wind(
     val speed: Double
 )
