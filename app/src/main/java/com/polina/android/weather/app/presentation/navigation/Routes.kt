bf2e567b0e96a2b728cd02e598945d4d3620face
polina <ff@gmail.com>
2025-04-08 23:09:02 +0200

    added Navigation

---
+++ app/src/main/java/com/polina/android/weather/app/presentation/navigation/Routes.kt
@@ -0,0 +1,15 @@
+package com.polina.android.weather.app.presentation.navigation
+
+object Routes {
+    const val MAIN_SCREEN = "main_screen"
+    const val DETAILS_SCREEN = "details_screen/{cityName}"
+    const val ERROR_SCREEN = "error_screen/{errorMessage}"
+
+    fun createDetailsRoute(cityName: String): String {
+        return "details_screen/$cityName"
+    }
+
+    fun createErrorRoute(errorMessage: String): String {
+        return "error_screen/$errorMessage"
+    }
+}
