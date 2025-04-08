e0fb3e8961d00e4abbcb27441dabab7c5450e15c
polina <ff@gmail.com>
2025-04-08 22:18:18 +0200

    small fixes

--- app/src/main/java/com/polina/android/weather/app/data/di/NetworkModule.kt
+++ app/src/main/java/com/polina/android/weather/app/data/di/NetworkModule.kt
@@ -1,22 +1,41 @@
 package com.polina.android.weather.app.data.di
 
 import com.polina.android.weather.app.data.api.WeatherApi
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     @Provides
     @Singleton
     fun provideWeatherApi(): WeatherApi {
         return Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/")
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(WeatherApi::class.java)
     }
